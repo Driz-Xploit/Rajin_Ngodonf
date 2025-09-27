2bc860f5cf307fe1703a39aa67438330620ff174
driz <driz@UNKNOWN.localhost>
2025-09-28 00:17:07 +0700

    Project 6: YTStream (Watch Yt video via Embed Yt + Local Video)

---
+++ Web-Development/Hari6/script.js
@@ -0,0 +1,50 @@
+getSrc('test.mp4', 'video')
+function whenError(args) {
+    let video = document.getElementById("videos")
+    video.innerHTML = "<h1 style='color: white;'>Something Error!: " + args + "<br>Please read <a href='help.html'>Guide</a> first</h1>"
+    video.style = "outline: solid 1px rgba(255, 255, 255, 0.2);"
+}
+// <Learning Area>
+document.getElementById('body').addEventListener('keydown', function(e) { if (e.key === "Enter") {getSrc(document.getElementById('searchs').value)} })
+function getSrc(temp_src, typeFrom="") {
+    if (temp_src.trim()) {
+        let video = document.getElementById("videos");
+        if (! typeFrom) {
+            typeFrom = document.querySelector('input[name="jenis"]:checked').value
+        }
+        if (temp_src.includes('"')) {
+            whenError("BlackList char detected");
+            return;
+        }
+
+        if (typeFrom === "video") {
+            temp_src = "test/" + temp_src
+            fetch(temp_src)
+            .then(res => {
+                if (!res.ok) throw new Error("Video tidak ditemukan");
+                return res.blob();
+            })
+            .then(blob => {
+                const url = URL.createObjectURL(blob);
+                document.getElementById("myVideo").src = url;
+            })
+            .catch(err => {
+                whenError(err.message)
+            });
+
+            video.innerHTML =
+            '<video controls src="' + temp_src + '" id="myVideo" loop></video>';
+        } else if (typeFrom === "iframe") {
+            if (temp_src.length == 11) {
+                video.innerHTML = `<iframe frameborder="0" src="https://youtube.com/embed/${temp_src}?autoplay=1&loop=1&playlist=${temp_src}" id="myIframe" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrer-policy="no-referrer" allowfullscreen></iframe>`;
+            } else {
+                whenError("Input Id embed tidak valid")
+                return
+            }
+        }
+        video.style = 'outline: none;'
+    } else {
+        whenError("Your Input pls...")
+    }
+}
+// <Learning Area>
